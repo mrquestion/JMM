5b12a49630847601af61dd26d1282b939e985b21
mrquestion <identific@naver.com>
2015-02-04 16:48:38 +0900

    modify

--- test/anissia/annisia_api_subs.py
+++ test/anissia/annisia_api_subs.py
@@ -3,26 +3,13 @@
 import os, sys
 import re
 import json
-#import socket
-#socket.INADDR_ANY = "0.0.0.0"
 import requests as rq
-#from urllib.parse import urlparse, urlunparse, parse_qs
-#from bs4 import BeautifulSoup
 from lxml import etree
 etree.namespaces = dict(re="http://exslt.org/regular-expressions")
-
-#import hashlib
-#def md5(s):
-#   e = hashlib.md5(str(s).encode("utf-8"))
-#   return e.hexdigest()
 
 import time, datetime
 def timestamp(format="%Y%m%d-%H%M%S", time=time.time(), rfc=None):
     return datetime.datetime.fromtimestamp(time).strftime(format)
-
-#import ctypes
-#def alert(s, title="Test"):
-#    ctypes.windll.user32.MessageBoxW(0, str(s), title, 0)
 
 import inspect, pprint
 #def args(f): return inspect.getargspec(f)
@@ -32,13 +19,7 @@ def pf(s): return pprint.pformat(s, indent=4, width=80)
 #def ppargs(f): pp(args(f))
 #def ppmems(o): pp(mems(o))
 
-def fix_indent(s):
-    s = re.sub(r"(\[)", "\\1\n ", s)
-    s = re.sub(r"(\])", "\n\\1", s)
-    s = re.sub(r"({)", "\\1\n     ", s)
-    s = re.sub(r"((\s*)(\s{4}))(.*)(},?)", "\\1\\4\\2\\5", s)
-    return s
-
+URL_FORMAT = "http://www.anissia.net/anitime/cap?i={}"
 URL_FORMAT = "http://www.anissia.net/anitime/list?w={}"
 DOTW = [ "[{}]".format(x) for x in [ "일", "월", "화", "수", "목", "금", "토" ] ]
 
@@ -46,20 +27,17 @@ def get_json_data(url):
     rs = rq.get(url)
     return json.loads(rs.text if rs.ok else "[]")
 
+def fix_indent(s):
+    s = re.sub(r"(\[)", "\\1\n ", s)
+    s = re.sub(r"(\])", "\n\\1", s)
+    s = re.sub(r"({)", "\\1\n     ", s)
+    s = re.sub(r"((\s*)(\s{4}))(.*)(},?)", "\\1\\4\\2\\5", s)
+    return s
+
 def test(filename='.'.join([ __file__, timestamp("%Y%m%d%H%M%S"), "txt" ])):
-    with open(filename, 'wb') as wo:
-        """ 0: Sunday,
-            1: Monday
-            ...
-            6: Saturday
-        """
-        for i in range(7):
-            wo.write(DOTW[i].encode())
-            wo.write(b'\n')
-            data = get_json_data(URL_FORMAT.format(i))
-            data = fix_indent(pf(data))
-            wo.write(data.encode())
-            wo.write(b'\n\n')
+    for i in range(6):
+        data = get_json_data(URL_FORMAT.format(i))
+        print(DOTW[i], len(data))
 
 def main(argc, args):
     test()
