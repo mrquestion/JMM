4cdc793b4fd6e1a0e17a1964a72ae4afe7434046
mrquestion <identific@naver.com>
2015-02-04 17:58:58 +0900

    test: annisia api - blogs of day

--- test/anissia/annisia_api_subs.py
+++ test/anissia/annisia_api_subs.py
@@ -19,12 +19,18 @@ def pf(s): return pprint.pformat(s, indent=4, width=80)
 #def ppargs(f): pp(args(f))
 #def ppmems(o): pp(mems(o))
 
-URL_FORMAT = "http://www.anissia.net/anitime/cap?i={}"
-DOTW = [ "[{}]".format(x) for x in [ "일", "월", "화", "수", "목", "금", "토" ] ]
-
-def get_json_data(url):
-    rs = rq.get(url)
-    return json.loads(rs.text if rs.ok else "[]")
+class objdict(dict):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+ 
+    def __getattr__(self, key):
+        return self[key] if key in self.keys() else None
+ 
+    def __setattr__(self, key, value):
+        self[key] = value
+ 
+    def __str__(self):
+        return "<objdict {}>".format(super().__str__())
 
 def fix_indent(s):
     s = re.sub(r"(\[)", "\\1\n ", s)
@@ -33,21 +39,23 @@ def fix_indent(s):
     s = re.sub(r"((\s*)(\s{4}))(.*)(},?)", "\\1\\4\\2\\5", s)
     return s
 
+URL_FORMAT1 = "http://www.anissia.net/anitime/list?w={}"
+URL_FORMAT2 = "http://www.anissia.net/anitime/cap?i={}"
+DOTW = [ "[{}]".format(x) for x in [ "일", "월", "화", "수", "목", "금", "토" ] ]
+
+def get_json_data(url):
+    rs = rq.get(url)
+    return json.loads(rs.text if rs.ok else "[]")
+
 def test(dotw, filename='.'.join([ __file__, timestamp("%Y%m%d%H%M%S"), "txt" ])):
-    with open(filename, 'wb') as wo:
-        """ 0: Sunday,
-            1: Monday
-            ...
-            6: Saturday
-        """
-		data = get_json_data(URL_FORMAT.format(dotw))
-        for i in range(7):
-            wo.write(DOTW[i].encode())
-            wo.write(b'\n')
-            data = get_json_data(URL_FORMAT.format(i))
-            data = fix_indent(pf(data))
-            wo.write(data.encode())
-            wo.write(b'\n\n')
+    result = [ DOTW[dotw%7], '' ]
+    w = [ objdict(x) for x in get_json_data(URL_FORMAT1.format(dotw)) ]
+    for a in w:
+        s = get_json_data(URL_FORMAT2.format(a.i)) # a["i"]
+        result.append("[{}]".format(a.s))
+        result.append(fix_indent(pf(s)))
+    with open(filename, "wb") as wbo:
+        wbo.write('\n'.join(result).encode())
 
 def main(argc, args):
     test(5)
